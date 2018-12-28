6ced0f59cad84f76c8c92354e5e16c8cccea0ad2
rrolt <rolletremi@gmail.com>
2018-12-28 21:46:27 +0100

    fetch results from service when search term changes

--- src/app/search/search.component.ts
+++ src/app/search/search.component.ts
@@ -1,5 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Output, EventEmitter } from '@angular/core';
 import { MoviesService } from '../core/services/movies.service';
+import { FormBuilder } from '@angular/forms';
+import { filter, debounceTime, switchMap } from 'rxjs/operators';
 
 @Component({
   selector: 'app-search',
@@ -7,7 +9,20 @@ import { MoviesService } from '../core/services/movies.service';
   styleUrls: ['./search.component.css']
 })
 export class SearchComponent implements OnInit {
-  constructor(private moviesService: MoviesService) {}
+  @Output() resultsChanged = new EventEmitter<any>();
+
+  searchForm = this.fb.group({ term: [''] });
+
+  constructor(private moviesService: MoviesService, private fb: FormBuilder) {
+    this.searchForm
+      .get('term')
+      .valueChanges.pipe(
+        debounceTime(500),
+        filter(term => term.length),
+        switchMap(term => this.moviesService.search(term))
+      )
+      .subscribe();
+  }
 
   ngOnInit() {}
 }
--- src/app/search/search.module.ts
+++ src/app/search/search.module.ts
@@ -2,13 +2,19 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { SearchComponent } from './search.component';
 import { MatFormFieldModule, MatInputModule } from '@angular/material';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, FormBuilder, ReactiveFormsModule } from '@angular/forms';
 import { MoviesService } from '../core/services/movies.service';
 
 @NgModule({
   declarations: [SearchComponent],
-  imports: [CommonModule, FormsModule, MatFormFieldModule, MatInputModule],
+  imports: [
+    CommonModule,
+    FormsModule,
+    MatFormFieldModule,
+    MatInputModule,
+    ReactiveFormsModule
+  ],
   exports: [SearchComponent],
-  providers: [MoviesService]
+  providers: [MoviesService, FormBuilder]
 })
 export class SearchModule {}
