f05a9ff908becff979d7f40129fe12354a2d590d
rrolt <rolletremi@gmail.com>
2019-01-01 21:56:36 +0100

    clean app component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,15 +1,8 @@
 import { Component } from '@angular/core';
-import { Movie } from './core/models/movies.model';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  movies: Movie[];
-
-  onMoviesChange(movies: Movie[]) {
-    this.movies = movies;
-  }
-}
+export class AppComponent {}
