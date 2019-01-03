1581d1db2ee1f9f585c7263bd1858c61b3f6be0f
rrolt <rolletremi@gmail.com>
2019-01-03 22:05:48 +0100

    show movie data on flip

--- src/app/movies/movies.module.ts
+++ src/app/movies/movies.module.ts
@@ -2,10 +2,11 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MoviesListComponent } from './movies-list/movies-list.component';
 import { MovieComponent } from './movie/movie.component';
+import { MatIconModule } from '@angular/material';
 
 @NgModule({
   declarations: [MoviesListComponent, MovieComponent],
-  imports: [CommonModule],
+  imports: [CommonModule, MatIconModule],
   exports: [MoviesListComponent, MovieComponent]
 })
 export class MoviesModule {}
