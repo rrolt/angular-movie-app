ba7526c344fabb7c7834aa890ab4b37de08e23a8
rrolt <rolletremi@gmail.com>
2019-01-06 15:23:54 +0100

    add firebase

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,5 +2,13 @@ export const environment = {
   production: true,
   omdb: {
     apiKey: 'a5cee388'
+  },
+  firebase: {
+    apiKey: 'AIzaSyBEvXEvEmwubv2lTAsQpsFvfAxvfmJCGRk',
+    authDomain: 'angular-movie-app-b3b62.firebaseapp.com',
+    databaseURL: 'https://angular-movie-app-b3b62.firebaseio.com',
+    projectId: 'angular-movie-app-b3b6',
+    storageBucket: 'angular-movie-app-b3b62.appspot.com',
+    messagingSenderId: '1081610173002'
   }
 };
